f4c9171a911e5b90f15574ec658309c564cc59f5
Zach <itsaboy3366@gmail.com>
2024-03-09 14:06:20 -0500

    removed unused a tag for logo

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -25,10 +25,10 @@ export default function Header() {
         aria-label="Global"
       >
         <div className="flex lg:flex-1">
-          <a href="#" className="-m-1.5 p-1.5">
+          <div href="#" className="-m-1.5 p-1.5">
             <span className="sr-only">Your Company</span>
             <img className="h-8 w-auto" src={logo} alt="" />
-          </a>
+          </div>
         </div>
         <div className="flex lg:hidden">
           <button
@@ -166,10 +166,10 @@ export default function Header() {
         <div className="fixed inset-0 z-50" />
         <Dialog.Panel className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-slate-950 px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10 bg-clip-padding backdrop-filter backdrop-blur-xl bg-opacity-20">
           <div className="flex items-center justify-between">
-            <a href="#" className="-m-1.5 p-1.5">
+            <div href="#" className="-m-1.5 p-1.5">
               <span className="sr-only">Your Company</span>
               <img className="h-10 w-auto" src={logo} alt="logo" />
-            </a>
+            </div>
             <button
               type="button"
               className="-m-2.5 rounded-md p-2.5 text-slate-200"
