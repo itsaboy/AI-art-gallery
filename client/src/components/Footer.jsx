4919bd1260e20f2edf22b095ab81c83af29fd0eb
Zach <itsaboy3366@gmail.com>
2024-03-09 13:57:07 -0500

    fixed footer

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,8 +1,8 @@
 export default function Footer() {
   return (
     <footer className="mx-auto max-w-7xl overflow-hidden px-6 pb-20 mt-2 sm:pb-24 lg:px-8">
-      <p className="mt-10 text-center text-xs leading-5 text-gray-500">
-        &copy; 2020 Your Company, Inc. All rights reserved.
+      <p className="mt-10 text-center text-xs leading-5 text-slate-200">
+        &copy; 2024 Zach of All Trades, Inc. All rights reserved.
       </p>
     </footer>
   );
