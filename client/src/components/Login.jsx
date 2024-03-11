606ba66086d9b1bccffc484bbfc07d6251985cfb
Zach <itsaboy3366@gmail.com>
2024-03-10 22:12:36 -0400

    disbaled user login and signup buttons until functional

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -56,13 +56,13 @@ export default function Login() {
               <br />
 
               <div>
-                <button
+                {/* <button
                   type="submit"
                   className="flex w-full justify-center rounded-md bg-blue-400 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-blue-600 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600"
                   disbaled
                 >
                   Log in
-                </button>
+                </button> */}
               </div>
             </form>
           </div>
